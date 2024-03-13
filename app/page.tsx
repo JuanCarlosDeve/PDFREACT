7b868abc2d514b82b1fe3e0d835c89769bd77098
Juan Carlos <juancarlosvigar@gmail.com>
2024-03-12 22:22:31 -0500

    primer commit con error

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,24 @@
 import { Container, Col } from "@citrica/objects";
 import Card from "@/components/citrica-ui/card";
-import Navbar from "@/components/citrica-ui/navbar";
+import PDFFile from "@/components/citrica-ui/pdf";
+import { PDFDownloadLink } from "@react-pdf/renderer"
 
 export default function Home() {
 	return (
 		<>
-			<Navbar/>
+
 			<section className="hero-section">
+				<PDFDownloadLink document={<PDFFile />} fileName="FORM">
+					{({ loading }) =>
+						loading ? (
+							<button> Loading Document...</button>
+						) : (
+							<button>Download</button>
+						)
+					}
+				</PDFDownloadLink>
 				<h1 className="display"> DISPLAY TEXT</h1>
+				<PDFFile />
 			</section>
 		</>
 	);
